4ac3682571fe3b9aca7d6befd9346a2214a7e8fa
Unknown <zum.q@mail.ru>
2019-02-20 19:51:30 +0500

    Добавлен генератор блоков.

---
+++ AndroidGame3/Assets/Scripts/GeneratorBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorBlock : MonoBehaviour {
+    public GameObject Prefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void CreateCube()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject cube = Instantiate(Prefab);
+            cube.transform.SetParent(transform);
+            cube.transform.position = new Vector3(Random.Range(-9f, 9f), Random.Range(5, 10), Random.Range(-19f, 19f));
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
