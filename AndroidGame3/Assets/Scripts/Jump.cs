bd7774f5a059f2bc986ebc6b22b1050276bb8b3c
Unknown <zum.q@mail.ru>
2019-03-23 21:19:56 +0500

    Добавлены прыжки.

---
+++ AndroidGame3/Assets/Scripts/Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public int time, timer;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+        if (timer > 0) timer--;
+        else
+        {
+            timer = time;
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up*force);
+        }
+    }
+}
