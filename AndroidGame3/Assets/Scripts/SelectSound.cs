8c933a63fe1405a82ff96283e99ba8d8f7d15871
Unknown <zum.q@mail.ru>
2019-03-30 04:35:54 +0500

    Выключен звук блоков.Закончена система звука.

---
+++ AndroidGame3/Assets/Scripts/SelectSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectSound : MonoBehaviour
+{
+    public GameObject pic1, pic2;
+    public void buttonTap()
+    {
+        GameController.MusicActive = !GameController.MusicActive;
+        pic1.SetActive(!pic1.activeSelf);
+        pic2.SetActive(!pic2.activeSelf);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
