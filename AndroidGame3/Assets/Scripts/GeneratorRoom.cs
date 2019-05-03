c771e1a6f2c912be982f3828f0c0149eeca32fd8
Unknown <zum.q@mail.ru>
2019-05-03 16:07:26 +0500

    Добавлен генератор комнат. И тестовая сцена.

--- AndroidGame3/Assets/Scripts/GeneratorRoom.cs
+++ AndroidGame3/Assets/Scripts/GeneratorRoom.cs
@@ -8,7 +8,7 @@ public class GeneratorRoom : MonoBehaviour
     {
        public string objBlock;
        public int length, width, difficult;
-
+       
        public Block(string objBlock, int length, int width, int difficult )
        {
             this.objBlock = objBlock;
@@ -18,19 +18,18 @@ public class GeneratorRoom : MonoBehaviour
        }
 
     }
-    
 
+    
     public int[] sizeX;
     public int[] sizeY;
     int[,] map = new int[14, 14];
     
     public GameObject Room;
-
+    public GameObject RoomPref;
     public TextAsset patternsText;
-    List<int[,]> patterns = new List<int[,]>();
-
+    List<int[,]> patterns = new List<int[,]>();  
     public TextAsset blockText;
-    List<Block>[] blocks = new List<Block>[10];
+    List<Block>[] blocks = new List<Block>[11];
     int[] Distribution = new int[] { 5, 15, 60, 15, 5 };
     private void Start()
     {
@@ -43,18 +42,29 @@ public class GeneratorRoom : MonoBehaviour
     public void GenerationRoom(int LvlRoom)
     {
         int difficult = difficultF(LvlRoom);
-
-         
-
-        List<GameObject> obj = new List<GameObject>();
-
-        
+        int[,] pattern = generationPattern(difficult);
+        //Debug.Log(pattern.Length);
+        int[] difficultBlocks = generationDifficultBlocks(pattern,difficult);
+        //Debug.Log(pattern.Length);
+        Block[] blocks = generationBlocks(difficultBlocks, pattern);       
+        CreateRoom(blocks,pattern);
 
     }
 
-    public void CreateRoom()
+    public void CreateRoom(Block[] blocks,int[,] pattern)
     {
-
+        if (Room != null) Destroy(Room);
+        GameObject roomObj = Instantiate(RoomPref) as GameObject;
+        roomObj.transform.position = RoomPref.transform.position;
+        Room = roomObj;
+        for (int i = 0; i< blocks.Length;i++)
+        {
+            //Debug.Log("Blocks/" + blocks[i].objBlock + " " + blocks[i].length + " " + blocks[i].width + " " + blocks[i].difficult);
+            GameObject blockObj = Instantiate(Resources.Load("Blocks/" + blocks[i].objBlock)) as GameObject;
+            blockObj.transform.SetParent(Room.transform);
+            Debug.Log((int)(Math.Abs(pattern[0, i] + pattern[2, i]) / 2)+" "+ 0 + " " + (int)(Math.Abs(pattern[1, i] + pattern[3, i]) / 2));
+            blockObj.transform.position = new Vector3((int)(Math.Abs(pattern[0, i] + pattern[2, i]) / 2), 0, (int)(Math.Abs(pattern[1, i] + pattern[3, i]) / 2));
+        }  
     }
 
     int difficultF(int LvlRoom)
@@ -66,7 +76,8 @@ public class GeneratorRoom : MonoBehaviour
 
     int[,] generationPattern(int difficult)
     {
-        return new int[0,0];
+        
+        return patterns[UnityEngine.Random.Range(0,patterns.Count)];
     }
 
     public Block[] generationBlocks(int[] difficultsBlock,int[,] pattern)
@@ -84,36 +95,64 @@ public class GeneratorRoom : MonoBehaviour
             else diffMin = diff - 2;
 
             int[] sizeBlock = new int[] { Math.Abs(pattern[0, i] - pattern[2, i]), Math.Abs(pattern[1, i] - pattern[3, i]) };
-            
 
             List<Block[]> listarraysBlocks = new List<Block[]>();
             int summBlocks = 0;
-            for (int j = diffMin; j < diffMax; j++)
+            for (int j = diffMin; j <= diffMax; j++)
             {
+                
                 Block[] AddingBlock = findBlocksSize(sizeBlock[1], sizeBlock[0], j);
                 listarraysBlocks.Add(AddingBlock);
                 summBlocks += AddingBlock.Length;
             }
 
-            float[] percentBlocks = new float[5];
-            float summDistributionBlocks = 0;
-            for (int j = 0; j < 5; j++)
+            if (summBlocks == 0)
             {
-                percentBlocks[j] = listarraysBlocks[j].Length / (100f * summBlocks) * Distribution[j];
-                summDistributionBlocks += percentBlocks[j];
+                Debug.Log("Zero " + sizeBlock[1] + " " + sizeBlock[0] + " " + diff);
+            }
+            for (int j = 0; j <= diffMax - diffMin; j++)
+            {
+                Debug.Log("Block " + sizeBlock[1] + " " + sizeBlock[0] + " " + listarraysBlocks[j].Length + " " +j);
             }
 
-            float[] chance = new float[5];
-            for (int j = 0; j < 5; j++)
+            float[] percentBlocks = new float[5];
+            float summDistributionBlocks = 0;
+            for (int j = 0; j <= diffMax - diffMin; j++)
             {
-                chance[j] = percentBlocks[i] / summDistributionBlocks * 100f;
+                int dist = Distribution[j];
+                if (diff == 0)
+                {                    
+                    dist = Distribution[j + 2];
+                }
+                if (diff == 1)
+                {
+                    dist = Distribution[j + 1]; ;
+                }
+                if (summBlocks != 0) percentBlocks[j] = listarraysBlocks[j].Length * 100f * dist / summBlocks ;
+                else percentBlocks[j] = 0;
+                //Debug.Log((listarraysBlocks[j].Length / summBlocks * 100f * dist) + " " + listarraysBlocks[j].Length / summBlocks);
+                summDistributionBlocks += percentBlocks[j];
+                Debug.Log("PercentBlocks " + percentBlocks[j]);
             }
             
-            float rand = UnityEngine.Random.Range(0f,100f);
-            for (int j = 0; j < 5; j++)
+            float[] chance = new float[5];
+            for (int j = 0; j <= diffMax - diffMin; j++)
             {
-                rand -= chance[j];
-                if (rand <= 0 || j == 4)
+                
+                chance[j] = percentBlocks[j] / (summDistributionBlocks + 1f) * 100f;
+                Debug.Log("Chance " +chance[j]);
+            }
+
+            
+            int rand = UnityEngine.Random.Range(1,100);
+            for (int j = 0; j <= diffMax - diffMin; j++)
+            {
+                
+                
+                Debug.Log(rand + " " + chance[j]);
+                rand -= (int)Math.Ceiling(chance[j]);
+                
+                if ((int)rand <= 0 || j == diffMax - diffMin)
                 {
                     blockRoom[i] = listarraysBlocks[j][UnityEngine.Random.Range(0, listarraysBlocks[j].Length)];
                     break;
@@ -129,7 +168,7 @@ public class GeneratorRoom : MonoBehaviour
     {
         int diff;
 
-        diff = (int)Math.Sqrt(difficult / pattern.Length);
+        diff = (int)Math.Sqrt(difficult * 4 / pattern.Length);
         if (diff > 10)
         {
             diff = 10;
@@ -154,7 +193,7 @@ public class GeneratorRoom : MonoBehaviour
         List<Block> blocksRet = new List<Block>();
         foreach (Block block in blocks[difficult])
         {
-            if ((block.length == length || block.length + 1 == length) && (block.width == width || block.width + 1 == width))
+            if ((block.length == length + 1 || block.length + 1 == length + 1) && (block.width == width + 1 || block.width + 1 == width + 1))
             {
                 blocksRet.Add(block);
             }
@@ -176,7 +215,8 @@ public class GeneratorRoom : MonoBehaviour
 
             for (int j = 0; j < lines.Length; j++)
             {
-                string[] splitLine = lines[i].Split(' ');
+                //Debug.Log(pattern.Length / 4 + " " + lines.Length + " " + patternsString.Length);
+                string[] splitLine = lines[j].Split(' ');
                 pattern[0, j] = int.Parse(splitLine[0]);
                 pattern[1, j] = int.Parse(splitLine[1]);
                 pattern[2, j] = int.Parse(splitLine[2]);
@@ -193,14 +233,24 @@ public class GeneratorRoom : MonoBehaviour
 
         string[] lines = alltext.Split('\n');
 
-        
+        for(int i = 0; i < 11; i++)
+        {
+            blocks[i] = new List<Block>();
+        }
         for (int i = 0; i < lines.Length; i++)
         {
             string[] splitLine = lines[i].Split(' ');
 
-            blocks[int.Parse(splitLine[3])].Add(new Block(splitLine[0],int.Parse(splitLine[1]), int.Parse(splitLine[2]), int.Parse(splitLine[3])));
+            
+            if (splitLine.Length == 4)
+            {
+                //Debug.Log(splitLine[0] + " " + splitLine[1] + " " + splitLine[2] + " " + splitLine[3]);
+                blocks[int.Parse(splitLine[3])].Add(new Block(splitLine[0], int.Parse(splitLine[1]), int.Parse(splitLine[2]), int.Parse(splitLine[3])));
+            }
+            
+
         }
-        Debug.Log("");
+        //Debug.Log("");
     }
 
 }
